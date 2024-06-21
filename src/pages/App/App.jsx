d246e7ccb894b131013ffb58a61c2a8e987a6d15
K0shal <gkmathscience@gmail.com>
2024-06-21 20:37:31 +0530

    pushed all changes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -104,7 +104,7 @@ function App() {
                 type="search"
                 id="default-search"
                 className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search Mockups, Logos..."
+                placeholder="Search...."
                 required
               />
               <button
@@ -227,7 +227,7 @@ function App() {
             onClick={() => {
               //if current page is 1 return
               if (currentPage <= 1) return;
-              console.log(currentPage, startPagination);
+          
               //if current page is greater than start pagination + 3 then decrease start pagination by 1
               if (currentPage < startPagination + 3 && startPagination > 0) {
            
